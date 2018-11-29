85337b8e5114c99669a8ff625f5e6861f4f181d1
grusscharles <grusscharles@gmail.com>
2018-11-29 21:37:06 +0100

    Scripts Breakable tiles

---
+++ Hexaflip/Assets/_Scripts/BreakableTile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableTile : MonoBehaviour {
+
+    [SerializeField]
+    GameObject hexaTriggerPrefab;
+
+    private void Awake()
+    {
+        GameObject hexaTrigger = Instantiate(hexaTriggerPrefab);
+        hexaTrigger.transform.SetParent(this.transform, true);
+    }
+}
---
+++ Hexaflip/Assets/_Scripts/TileTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTrigger : MonoBehaviour {
+    [Tooltip("Time before this tile falls")]
+    [SerializeField]
+    float lifeTime = 2f;
+
+    float t;
+    bool objectOnTile = false;
+    GameObject breakableTile;
+
+    void Awake ()
+    {
+        t = 0f;
+        breakableTile = transform.parent.gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    { 
+        objectOnTile = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        objectOnTile = false;
+    }
+
+    void Update ()
+    {
+        if (objectOnTile)
+        {
+            t += Time.deltaTime;
+        }
+
+        if(t > lifeTime)
+        {
+            Rigidbody rb = breakableTile.AddComponent<Rigidbody>();
+            rb.useGravity = true;
+            Invoke("DestroyTile", 2f);
+        }
+    }
+
+    private void DestroyTile()
+    {
+        Destroy(breakableTile);
+    }
+
+}
